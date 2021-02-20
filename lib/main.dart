113958886963d1866ced7006fe6a210cfeb4a3e9
Abhishek Bedi <kbedi@mafcarrefour.com>
2021-02-21 03:29:55 +0400

    Basic Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "My App",
+    theme: ThemeData(primarySwatch: Colors.purple),
     home: HomePage(),
   ));
 }
@@ -11,31 +12,10 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("App Title")),
-        body: Container(
-          color: Colors.black,
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(width: 100, height: 100, color: Colors.green),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.yellow,
-                ),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.red,
-                )
-              ],
-            ),
-          ),
-        ));
+      appBar: AppBar(title: Text("App Title")),
+      body: Container(color: Colors.yellow),
+      drawer: Drawer(child: Container(color: Colors.red)),
+    );
   }
 }
 
