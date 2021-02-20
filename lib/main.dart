1f518e3485d8ab9a1c6668d683298169899beb33
Abhishek Bedi <kbedi@mafcarrefour.com>
2021-02-21 03:14:44 +0400

    Container basics with alignment

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,50 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text("App Title")),
-        body: Container(child: Center(child: Text("Abhishek Bedi"))));
+        body: Container(
+          color: Colors.black,
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(width: 100, height: 100, color: Colors.green),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.red,
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
+
+/*
+
+Container(
+          width: 100,
+          height: 100,
+          child: Text('Box'),
+          decoration: BoxDecoration(
+              color: Colors.yellowAccent,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey[400],
+                    blurRadius: 5,
+                    offset: Offset(2, 5)),
+              ],
+              gradient: LinearGradient(colors: [Colors.yellow, Colors.pink])),
+          alignment: Alignment.center,
+        )
+        
+         */
