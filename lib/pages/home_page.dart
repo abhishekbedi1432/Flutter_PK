d12709694216d20283bceb4c9b7fe23490a2361c
Abhishek Bedi <kbedi@mafcarrefour.com>
2021-02-21 23:49:14 +0400

    List View Builder Sample Code

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/change_name_card.dart';
-import 'package:my_app/drawer.dart';
+// import 'package:my_app/change_name_card.dart';
+// import 'package:my_app/drawer.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,26 +9,23 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   var myText = 'Change Me';
-  TextEditingController _nameController = TextEditingController();
+
+  final List<String> entries = <String>['Abhishek', 'Bedi', 'C'];
+  final List<int> colorCodes = <int>[600, 500, 100];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text("App Title")),
-        body: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
-            child:
-                ChangeNameCard(myText: myText, nameController: _nameController),
-          ),
-        ),
-        drawer: MyDrawer(),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            myText = _nameController.text;
-            setState(() {});
-          },
-          child: Icon(Icons.edit),
-        ));
+        body: ListView.builder(
+            padding: const EdgeInsets.all(8),
+            itemCount: entries.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 50,
+                color: Colors.amber[colorCodes[index]],
+                child: Center(child: Text('Entry ${entries[index]}')),
+              );
+            }));
   }
 }
